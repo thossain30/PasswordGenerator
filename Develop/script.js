b44aa4689f583431643ec41601b85d4a88482703
thossain30 <54952959+thossain30@users.noreply.github.com>
2023-02-09 16:43:19 -0500

    Listed out steps needed for the Generator and implemented length prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,33 @@
 // Assignment code here
+function generatePassword() {
+  // 1. Prompt the user for criteria
+  //    a. length ( greater than 8 and less than 128)
+  // prompts for length
+  let lengthPrompt = prompt("Enter your desired password length (8 - 128 characters)");
+  // converts lengthPrompt into a number
+  let length = parseInt(lengthPrompt);
+  // Checks if length fits the criteria and is a number
+  while (length < 8 || length > 128 || length == NaN) {
+    length = prompt("Please enter a number between 8 and 128");
+  }
+  //    b. Char types (uppercase, lowercase, special, etc.)
+  // 2. Validate the input 
+  // 3. Generate password based on criteria
 
+
+  // 4. Display password to the page
+  let result = "password will go here";
+  return result;
+}
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+// This let refers to the button at the bottom
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
