e1ec698b959dd948953eb1bbd8ae88ebbb604846
thossain30 <54952959+thossain30@users.noreply.github.com>
2023-02-10 14:48:22 -0500

    Added password generation and have it display on page

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,31 @@
 // Assignment code here
 function generatePassword() {
+
+  // Different char sets that can be used for password generation
   let upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   let lowerChars = upperChars.toLowerCase();
   let numbers = "0123456789"
-  let specialChars = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  let specialChars = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~" + '"';
 
+  // Different confirms to check whether each char set is valid for our password
   let isLower;
   let isUpper;
   let isNumber;
   let isSpecial;
   // 1. Prompt the user for criteria
   //    a. length ( greater than 8 and less than 128)
+
   // prompts for length
   let lengthPrompt = prompt("Enter your desired password length (8 - 128 characters)");
+  
   // converts lengthPrompt into a number
   let length = parseInt(lengthPrompt);
+  
   // Checks if length fits the criteria and is a number
   while (length < 8 || length > 128 || length == NaN) {
     length = prompt("Please enter a number between 8 and 128");
   }
+  
   //    b. Char types (uppercase, lowercase, special, etc.)
   // repeats these prompts until at least one char type is true;
   do {
@@ -26,10 +33,13 @@ function generatePassword() {
     isUpper = confirm("Would you like uppercase letters?");
     isNumber = confirm("Would you like numbers?");
     isSpecial = confirm("And finally, would you like special characters?");
-  } while (!isLower && !isUpper && !isNumber && !isSpecial )
+  } while (!isLower && !isUpper && !isNumber && !isSpecial)
   // 2. Validate the input
 
-  // The set of characters valid for the input
+  /* The set of characters valid for the input 
+    If the return value from each confirm is true (user clicked OK),
+    Then we add the respective characters from each set into our string of valid chars
+  */
   let chars = "";
   if (isLower) {
     chars += lowerChars;
@@ -43,17 +53,22 @@ function generatePassword() {
   if (isSpecial) {
     chars += specialChars;
   }
-
-  console.log("chars: " + chars);
   // 3. Generate password based on criteria
+  let result = "";
 
+  /*
+    While the length of our result is less than length prompted,
+    We add a random character from our chars String to result
+    (Math.floor ensures we only get ints up to and including length of chars)
+  */
+  while (result.length < length) {
+    result += chars[Math.floor(Math.random() * chars.length)];
+  }
 
   // 4. Display password to the page
-  let result = "password will go here";
   return result;
 }
-// Get references to the #generate element
-// This let refers to the button at the bottom
+// Get references to the #generate element (Button at the bottom)
 let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
